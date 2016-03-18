84971a93a56bf7a5811baaf2da92e757db750ae1
Jose Miguel <jm@0pt1mates.com>
2016-03-18 15:47:02 +0000

    STEP 2: apply offers.

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -6,14 +6,14 @@ class ShoppingCartSpec extends Specification {
   def is =
     s2"""
          Shopping cart checkout
-          an apple is '£0.60'                                                 $e1
-          an orange is '£0.25'                                                $e2
-          cost for 'Apple,Orange,Apple,Apple' is 2.05                         $e3
-          cost for invalid input is error message                             $e4
+          an apple is 60p                                                 $e1
+          an orange is 25p                                                $e2
+          cost for 'Apple,Orange,Apple,Apple' is 2.05 after offer is 1.45 $e3
+          cost for invalid input is error message                         $e4
   """
 
   def e1 = new ShoppingCart("Apple").total() must be equalTo ("£0.60")
   def e2 = new ShoppingCart("Orange").total() must be equalTo ("£0.25")
-  def e3 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo ("£2.05")
+  def e3 = new ShoppingCart("Apple,Orange,Apple,Apple").total() must be equalTo ("£1.45")
   def e4 = new ShoppingCart("Apple,Pear,XXX,Apple").total() must be equalTo ("ERROR: invalid input.")
 }
