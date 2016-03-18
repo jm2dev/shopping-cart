c5288a6f942119664c5455a2185845791a200371
Jose Miguel <jm@0pt1mates.com>
2016-03-18 15:54:05 +0000

    Notes on using the repl.

--- src/test/scala/OrangeSpec.scala
+++ src/test/scala/OrangeSpec.scala
@@ -6,9 +6,9 @@ class OrangeSpec extends Specification {
   def is =
     s2"""
       Cost
-        an orange costs '£0.25'  $e1
-        two oranges cost '£0.50' $e2
-        three oranges cost '£0.50' $e3
+        an orange costs '£0.25'     $e1
+        two oranges cost '£0.50'    $e2
+        three oranges cost '£0.50'  $e3
   """
 
   def e1 = new Orange(1).total() should be equalTo("£0.25")
